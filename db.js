66910a7ebd6191698a6ebf2dd16ff31f417d8a7d
RoshanKajulkar <roshankajulkar@gmail.com>
2026-03-22 15:42:34 +0530

    minor fixes

--- db.js
+++ db.js
@@ -6,10 +6,6 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
 });
 
 pool.on("connect", () => {
